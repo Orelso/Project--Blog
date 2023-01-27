27506d3e7c56f6e2d683781bc1495ce51d907c60
Orelso Vitam <orelso.vitam@avanio.com>
2023-01-27 12:33:27 +0200

    cleaned the app & added some javascript

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,31 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setName] = useState("Mario"); // Creates a storage for the name
+  const [age, setAge] = useState(25)
 
-    const handleClick = (e) => {
-        console.log("hello O", e)
-    }
+  const handleClick = () => {
+    setName("Luigi"); // When setName is called inside the handleClick it changes the name to the NEW setName
+    setAge(100)
+  };
 
-    const handleClickAgain = (name) => {
-        console.log(`hello ${name}`)
-    }
-    return ( 
-        <div className="home">
-            <h1>Homepage</h1>
-            <button onClick={handleClick}>Click me!</button>
-            <button onClick={() => handleClickAgain("mario")}>Click me again!</button>
+  const handleClickAgain = () => {
+    setName('Orelso')
+    setAge(31)
 
-        </div>
-     );
-}
- 
+  };
+  return (
+    <div className='home'>
+      <h1>Homepage</h1>
+      <p>
+        <span className="nameStyle">{name}</span> is {age} years old
+        </p>
+      <container>
+      <button onClick={handleClick}>Click me!</button>
+      <button onClick={() => handleClickAgain("mario")}>Click me again!</button>
+      </container>
+    </div>
+  );
+};
+
 export default Home;
