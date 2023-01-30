c198a18b692939ea1bf6ef3742cf40478464df70
Orelso Vitam <orelso.vitam@avanio.com>
2023-01-30 15:19:01 +0200

    useEffect

---
+++ src/BlogList.js
@@ -0,0 +1,59 @@
+// * calling the blogs={blogs} prop from the Home.js file
+const BlogList = ({blogs, title}) => {
+  
+    
+    return ( 
+        <div className='blog-listme'>
+            <h1>{title}</h1>
+        <ul>
+           {blogs.map((blog) => (
+               <div className="blog-preview" key={blog.id}>
+                   <h2>{blog.title}</h2>
+                   <h3>{blog.body}</h3>
+                   <p>Written by: {blog.author}</p>
+               </div>
+           ))}
+        </ul>
+       </div>
+     );
+}
+ 
+export default BlogList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//! calling the blogs={blogs} prop from the Home.js file with props
+// const BlogList = (props) => {
+//     const blogs = props.blogs
+//     console.log(props, blogs);
+//     const title = props.title
+//     return ( 
+//         <div className='blog-listme'>
+//             <h1>{props.title}</h1>
+//         <ul>
+//            {blogs.map((blog) => (
+//                <div className="blog-preview" key={blog.id}>
+//                    <h2>{blog.title}</h2>
+//                    <h3>{blog.body}</h3>
+//                    <p>{blog.author}</p>
+//                </div>
+//            ))}
+//         </ul>
+//        </div>
+//      );
+// }
--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {title: "Music", body: "lorem", author: "Orelso", id: 1},
-    {title: "Food", body: "lorem", author: "Sanni", id: 2},
-    {title: "Leisure", body: "lorem", author: "Irene", id: 3}
-  ])
+  const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true)
 
-  const handleClick = () => {
-    setBlogs()
-  }
+  useEffect(() => { //4 ran at every render.
+   setTimeout(() => {
+    fetch('http://localhost:8005/blogs')
+    .then(res => {
+        return res.json()
+    })
+    .then((data) => {
+        setBlogs(data)
+        setIsPending(false)
+    })
+   }, 1000)
+  }, []) //4 [] makes sure this useeffect only runs after the first initial render.
  
   return (
     <div className='home'>
-     <ul>
-        {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <h3>{blog.body}</h3>
-                <p>{blog.author}</p>
-            </div>
-        ))}
-     </ul>
+      {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>} 
+        {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Irene')} title="Irene's Blogs!" />  */}
     </div>
   );
 };
