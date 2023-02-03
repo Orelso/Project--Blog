1aaf82a35f72a84531094b607b618fa90fecaa25
Orelso Vitam <orelso.vitam@avanio.com>
2023-02-03 11:22:56 +0200

    home

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,11 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
-  const [isPending, setIsPending] = useState(true)
-  const [error, setError] = useState(null)
 
-  useEffect(() => { //4 ran at every render.
-   setTimeout(() => {
-    fetch('http://localhost:8005/blogs')
-    .then(res => {
-      if(!res.ok) {
-        throw Error('Could not fetch the data :(')
-      }
-        return res.json()
-    })
-    .then((data) => {
-        setBlogs(data)
-        setIsPending(false)
-        setError(null)
-    })
-    .catch(err => {
-      setIsPending(false)
-      setError(err.message)
-    })
-   }, 1000)
-  }, []) //4 [] makes sure this useeffect only runs after the first initial render.
- 
+  const {data: blogs, isPending, error} = useFetch("http://localhost:8005/blogs")
+
   return (
     <div className='home'>
       { error && <div>{ error }</div>}
