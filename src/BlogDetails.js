674294ff9673fd2030d363e21443675e13509f4f
Orelso Vitam <orelso.vitam@avanio.com>
2023-02-03 14:30:42 +0200

    Create.js

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const {id} = useParams()
+    const {data: blog, error, isPending} = useFetch(`http://localhost:8005/blogs/` + id)
     return ( 
-        <div className="blod-details">
-            <h2>Blog Details - {id} </h2>
-
+        <div className="blog-details">
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            { blog && (
+                <article>
+                    <h1> {blog.title}</h1>
+                    <h3> {blog.body}</h3>
+                    <h4>Written By: {blog.author}</h4>
+                </article>
+            )}
         </div>
      );
 }
